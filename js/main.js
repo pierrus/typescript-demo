3b8b955c2cd991323bc91485b5f71044e9a4baee
Pierre Murasso <Pierre@MacBook-Pro-de-Pierre.local>
2016-11-17 12:10:12 +0100

    Reorganized JS and TS files

--- js/main.js
+++ js/main.js
@@ -1,11 +1,10 @@
 /**
  * Created by Pierre on 16/11/2016.
  */
-/// <reference path="node_modules/@types/jquery/index.d.ts" />
+/// <reference path="../node_modules/@types/jquery/index.d.ts" />
 define(["require", "exports", "./orders-model", "./orders-service"], function (require, exports, orders_model_1, orders_service_1) {
     "use strict";
     $(document).ready(function () {
-        var s = 'Hello';
         var orderService = new orders_service_1.OrderService();
         var p = new orders_model_1.Product("Macbook PRO", 3200, 3);
         var p2 = new orders_model_1.Product("NIKE Air Force 1", 130, 1);
--- ts/main.ts
+++ ts/main.ts
@@ -1,16 +1,13 @@
 /**
  * Created by Pierre on 16/11/2016.
  */
-/// <reference path="node_modules/@types/jquery/index.d.ts" />
+/// <reference path="../node_modules/@types/jquery/index.d.ts" />
 
 import {Product} from "./orders-model";
 import {OrderService, IOrderService} from "./orders-service";
 
 $(document).ready(function() {
-    let s: string = 'Hello';
-
     let orderService: IOrderService = new OrderService();
-
 
     let p:Product = new Product("Macbook PRO", 3200, 3);
     let p2:Product = new Product("NIKE Air Force 1", 130, 1);
